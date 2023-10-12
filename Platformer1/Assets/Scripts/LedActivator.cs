127b15d7645bb5e5104602f25fe6debaf2bc8743
Carlos Estay <75865555+carlosestay@users.noreply.github.com>
2023-10-11 20:19:40 -0600

    Update LedActivator.cs
    - Improved led activation detection

--- Platformer1/Assets/Scripts/LedActivator.cs
+++ Platformer1/Assets/Scripts/LedActivator.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LedActivator : MonoBehaviour
 {
-    public static int LedCount;
+    public const byte RED_LED = 0x4;
+    public const byte YELLOW_LED = 0x2;
+    public const byte GREEN_LED = 0x1;
+    public const byte ALL_LED = RED_LED | YELLOW_LED | GREEN_LED;
+
+
+
+    public static byte LedCount = 0;
+
 
     [SerializeField]
     GameObject flashlight, halo;
@@ -49,27 +58,52 @@ public class LedActivator : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             command = string.Empty;
+            prevState = state;
             StartCoroutine("ToggleState");
             itemLight.intensity = state * onIntensity;
             command += ledInitial;
             if (state == 1)
             {
                 halo.SetActive(true);
-                LedCount++;
                 transform.localScale = new Vector3(0.04f, yScaleON, 0.04f);
                 command += '1';
             }
             else
             {
-                halo.SetActive(false);
-                LedCount--;      
+                halo.SetActive(false);   
                 transform.localScale = new Vector3(0.04f, yScaleOFF, 0.04f);
                 command += '0';
             }
-            command += '\r';
-            print(LedCount);
+            //command += '\r';
+
+            byte mask = 0;
+            switch (ledInitial)
+            {
+                case 'R':
+                    mask = RED_LED;
+                    break;
+                case 'Y':
+                    mask = YELLOW_LED;
+                    break;
+                case 'G':
+                    mask = GREEN_LED;
+                    break;
+
+                default:
+                    break;
+            }
+            if(state == 1)
+            {
+                LedCount |= mask;
+            }
+            else
+            {
+                LedCount &= (byte)~mask;
+            }
+
+            print("LED COUNT: " + LedCount);
             EventManager.Instance.onLedActivator.Invoke(gameObject, new CustomEventArgs(command));
-            if(LedCount == 3)
+            if(LedCount == ALL_LED)
             {
                 EventManager.Instance.onGameEnd.Invoke(gameObject, new CustomEventArgs(true));
             }
@@ -78,8 +112,11 @@ public class LedActivator : MonoBehaviour
 
     IEnumerator ToggleState()
     {
-        yield return new WaitForSeconds(0.5f);
-        state ^= 1;
+        yield return new WaitForSeconds(0.1f);
+        if(state == prevState)
+        {
+            state ^= 1;
+        }
     }
 
 }
